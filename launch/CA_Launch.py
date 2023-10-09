cdb073cd842c9d8905a9d546a4a6ef3cbaa680dc
swarm <liewyungjun@gmail.com>
2023-10-09 16:26:25 +0800

    launch file to use cpp backend

--- launch/CA_Launch.py
+++ launch/CA_Launch.py
@@ -2,10 +2,7 @@ import os
 import yaml
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.conditions import LaunchConfigurationEquals
-from launch.conditions import LaunchConfigurationNotEquals
 from launch.substitutions import LaunchConfiguration, PythonExpression
 
 
@@ -22,16 +19,13 @@ def generate_launch_description():
     server_params = crazyflies
 
     return LaunchDescription([
-        DeclareLaunchArgument('backend', default_value='cpp'),
-        DeclareLaunchArgument('debug', default_value='False'),
-
         Node(
             package='crazyflie',
             executable='crazyflie_server',
-            condition=LaunchConfigurationEquals('backend','cpp'),
             name='crazyflie_server',
             output='screen',
-            parameters=server_params,
-            prefix=PythonExpression(['"xterm -e gdb -ex run --args" if ', LaunchConfiguration('debug'), ' else ""']),
+            parameters=[server_params],
+            #prefix=PythonExpression(['"xterm -e gdb -ex run --args" if ', LaunchConfiguration('debug'), ' else ""']),
+
         ),
     ])
