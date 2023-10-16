4c72c6f6fdf91a90a73ebce687d6e31f3cf47011
liewyungjun <liewyungjun@gmail.com>
2023-10-16 08:30:53 +0800

    added takeoff service call

--- src/simpleCA.cpp
+++ src/simpleCA.cpp
@@ -31,8 +31,34 @@ class SimpleCA : public rclcpp::Node
             "/" + drone_ + "/topic_name1", 10, std::bind(&SimpleCA::range_callback, this, _1));
       publisher_ = this->create_publisher<crazyflie_interfaces::msg::Hover>(
             "/" + drone_ + "/cmd_hover", 10);
-      RCLCPP_INFO_STREAM(this->get_logger(), "initialised!");   
-    }
+      RCLCPP_INFO_STREAM(this->get_logger(), "initialised!"); 
+
+      takeoff_client_ = this->create_client<crazyflie_interfaces::srv::Takeoff>("takeoff");
+      // Wait for the takeoff service to be available
+      while (!takeoff_client_->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+          RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+          return;
+        }
+        RCLCPP_INFO(this->get_logger(), "Service not available, waiting again...");
+      }
+
+      // Call the takeoff service
+      auto request = std::make_shared<crazyflie_interfaces::srv::Takeoff::Request>();
+      request->height = height_;
+      request->duration = 2.0;
+      auto result = takeoff_client_->async_send_request(request);
+      if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) ==
+          rclcpp::executor::FutureReturnCode::SUCCESS)
+      {
+        RCLCPP_INFO(this->get_logger(), "Takeoff service succeeded");
+      } else {
+        RCLCPP_ERROR(this->get_logger(), "Takeoff service failed");
+      }
+
+
+        
+      }
 
   private:
     //to change msg type
@@ -69,7 +95,7 @@ class SimpleCA : public rclcpp::Node
     }
     rclcpp::Subscription<crazyflie_interfaces::msg::LogDataGeneric>::SharedPtr subscription_;
     rclcpp::Publisher<crazyflie_interfaces::msg::Hover>::SharedPtr publisher_;
-    //rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Client<crazyflie_interfaces::srv::Takeoff>::SharedPtr takeoff_client_;
     float threshold_;
     float height_;
     float speed_;
